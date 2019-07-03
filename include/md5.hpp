207a6401f5a7fd6ef4e8e99c7fe7d3a1210e7e8b
Elviss Strazdins <elviss@elviss.lv>
2019-07-03 12:45:15 +0300

    Fix casing of md5 functions

--- include/md5.hpp
+++ include/md5.hpp
@@ -33,48 +33,48 @@ namespace md5
     }
 
     // F, G, H and I are basic MD5 functions
-    constexpr uint32_t F(uint32_t x, uint32_t y, uint32_t z)
+    constexpr uint32_t f(uint32_t x, uint32_t y, uint32_t z)
     {
         return (x & y) | (~x & z);
     }
 
-    constexpr uint32_t G(uint32_t x, uint32_t y, uint32_t z)
+    constexpr uint32_t g(uint32_t x, uint32_t y, uint32_t z)
     {
         return (x & z) | (y & ~z);
     }
 
-    constexpr uint32_t H(uint32_t x, uint32_t y, uint32_t z)
+    constexpr uint32_t h(uint32_t x, uint32_t y, uint32_t z)
     {
         return x ^ y ^ z;
     }
 
-    constexpr uint32_t I(uint32_t x, uint32_t y, uint32_t z)
+    constexpr uint32_t i(uint32_t x, uint32_t y, uint32_t z)
     {
         return y ^ (x | ~z);
     }
 
-    constexpr uint32_t FF(uint32_t a, uint32_t b, uint32_t c, uint32_t d,
+    constexpr uint32_t ff(uint32_t a, uint32_t b, uint32_t c, uint32_t d,
                           uint32_t x, uint32_t s, uint32_t ac)
     {
-        return rotateLeft(a + F(b, c, d) + x + ac, s) + b;
+        return rotateLeft(a + f(b, c, d) + x + ac, s) + b;
     }
 
-    constexpr uint32_t GG(uint32_t& a, uint32_t b, uint32_t c, uint32_t d,
+    constexpr uint32_t gg(uint32_t& a, uint32_t b, uint32_t c, uint32_t d,
                           uint32_t x, uint32_t s, uint32_t ac)
     {
-        return rotateLeft(a + G(b, c, d) + x + ac, s) + b;
+        return rotateLeft(a + g(b, c, d) + x + ac, s) + b;
     }
 
-    constexpr uint32_t HH(uint32_t& a, uint32_t b, uint32_t c, uint32_t d,
+    constexpr uint32_t hh(uint32_t& a, uint32_t b, uint32_t c, uint32_t d,
                           uint32_t x, uint32_t s, uint32_t ac)
     {
-        return rotateLeft(a + H(b, c, d) + x + ac, s) + b;
+        return rotateLeft(a + h(b, c, d) + x + ac, s) + b;
     }
 
-    constexpr uint32_t II(uint32_t& a, uint32_t b, uint32_t c, uint32_t d,
+    constexpr uint32_t ii(uint32_t& a, uint32_t b, uint32_t c, uint32_t d,
                           uint32_t x, uint32_t s, uint32_t ac)
     {
-        return rotateLeft(a + I(b, c, d) + x + ac, s) + b;
+        return rotateLeft(a + i(b, c, d) + x + ac, s) + b;
     }
 
     inline void transform(uint32_t state[4], const uint8_t block[64])
@@ -88,73 +88,73 @@ namespace md5
                                          (block[i * 4 + 2] << 16) |
                                          (block[i * 4 + 3] << 24));
 
-        a = FF(a, b, c, d, x[0], S11, 0xD76AA478);
-        d = FF(d, a, b, c, x[1], S12, 0xE8C7B756);
-        c = FF(c, d, a, b, x[2], S13, 0x242070DB);
-        b = FF(b, c, d, a, x[3], S14, 0xC1BDCEEE);
-        a = FF(a, b, c, d, x[4], S11, 0xF57C0FAF);
-        d = FF(d, a, b, c, x[5], S12, 0x4787C62A);
-        c = FF(c, d, a, b, x[6], S13, 0xA8304613);
-        b = FF(b, c, d, a, x[7], S14, 0xFD469501);
-        a = FF(a, b, c, d, x[8], S11, 0x698098D8);
-        d = FF(d, a, b, c, x[9], S12, 0x8B44F7AF);
-        c = FF(c, d, a, b, x[10], S13, 0xFFFF5BB1);
-        b = FF(b, c, d, a, x[11], S14, 0x895CD7BE);
-        a = FF(a, b, c, d, x[12], S11, 0x6B901122);
-        d = FF(d, a, b, c, x[13], S12, 0xFD987193);
-        c = FF(c, d, a, b, x[14], S13, 0xA679438E);
-        b = FF(b, c, d, a, x[15], S14, 0x49B40821);
+        a = ff(a, b, c, d, x[0], S11, 0xD76AA478);
+        d = ff(d, a, b, c, x[1], S12, 0xE8C7B756);
+        c = ff(c, d, a, b, x[2], S13, 0x242070DB);
+        b = ff(b, c, d, a, x[3], S14, 0xC1BDCEEE);
+        a = ff(a, b, c, d, x[4], S11, 0xF57C0FAF);
+        d = ff(d, a, b, c, x[5], S12, 0x4787C62A);
+        c = ff(c, d, a, b, x[6], S13, 0xA8304613);
+        b = ff(b, c, d, a, x[7], S14, 0xFD469501);
+        a = ff(a, b, c, d, x[8], S11, 0x698098D8);
+        d = ff(d, a, b, c, x[9], S12, 0x8B44F7AF);
+        c = ff(c, d, a, b, x[10], S13, 0xFFFF5BB1);
+        b = ff(b, c, d, a, x[11], S14, 0x895CD7BE);
+        a = ff(a, b, c, d, x[12], S11, 0x6B901122);
+        d = ff(d, a, b, c, x[13], S12, 0xFD987193);
+        c = ff(c, d, a, b, x[14], S13, 0xA679438E);
+        b = ff(b, c, d, a, x[15], S14, 0x49B40821);
 
-        a = GG(a, b, c, d, x[1], S21, 0xF61E2562);
-        d = GG(d, a, b, c, x[6], S22, 0xC040B340);
-        c = GG(c, d, a, b, x[11], S23, 0x265E5A51);
-        b = GG(b, c, d, a, x[0], S24, 0xE9B6C7AA);
-        a = GG(a, b, c, d, x[5], S21, 0xD62F105D);
-        d = GG(d, a, b, c, x[10], S22,  0x2441453);
-        c = GG(c, d, a, b, x[15], S23, 0xD8A1E681);
-        b = GG(b, c, d, a, x[4], S24, 0xE7D3FBC8);
-        a = GG(a, b, c, d, x[9], S21, 0x21E1CDE6);
-        d = GG(d, a, b, c, x[14], S22, 0xC33707D6);
-        c = GG(c, d, a, b, x[3], S23, 0xF4D50D87);
-        b = GG(b, c, d, a, x[8], S24, 0x455A14ED);
-        a = GG(a, b, c, d, x[13], S21, 0xA9E3E905);
-        d = GG(d, a, b, c, x[2], S22, 0xFCEFA3F8);
-        c = GG(c, d, a, b, x[7], S23, 0x676F02D9);
-        b = GG(b, c, d, a, x[12], S24, 0x8D2A4C8A);
+        a = gg(a, b, c, d, x[1], S21, 0xF61E2562);
+        d = gg(d, a, b, c, x[6], S22, 0xC040B340);
+        c = gg(c, d, a, b, x[11], S23, 0x265E5A51);
+        b = gg(b, c, d, a, x[0], S24, 0xE9B6C7AA);
+        a = gg(a, b, c, d, x[5], S21, 0xD62F105D);
+        d = gg(d, a, b, c, x[10], S22,  0x2441453);
+        c = gg(c, d, a, b, x[15], S23, 0xD8A1E681);
+        b = gg(b, c, d, a, x[4], S24, 0xE7D3FBC8);
+        a = gg(a, b, c, d, x[9], S21, 0x21E1CDE6);
+        d = gg(d, a, b, c, x[14], S22, 0xC33707D6);
+        c = gg(c, d, a, b, x[3], S23, 0xF4D50D87);
+        b = gg(b, c, d, a, x[8], S24, 0x455A14ED);
+        a = gg(a, b, c, d, x[13], S21, 0xA9E3E905);
+        d = gg(d, a, b, c, x[2], S22, 0xFCEFA3F8);
+        c = gg(c, d, a, b, x[7], S23, 0x676F02D9);
+        b = gg(b, c, d, a, x[12], S24, 0x8D2A4C8A);
 
-        a = HH(a, b, c, d, x[5], S31, 0xFFFA3942);
-        d = HH(d, a, b, c, x[8], S32, 0x8771F681);
-        c = HH(c, d, a, b, x[11], S33, 0x6D9D6122);
-        b = HH(b, c, d, a, x[14], S34, 0xFDE5380C);
-        a = HH(a, b, c, d, x[1], S31, 0xA4BEEA44);
-        d = HH(d, a, b, c, x[4], S32, 0x4BDECFA9);
-        c = HH(c, d, a, b, x[7], S33, 0xF6BB4B60);
-        b = HH(b, c, d, a, x[10], S34, 0xBEBFBC70);
-        a = HH(a, b, c, d, x[13], S31, 0x289B7EC6);
-        d = HH(d, a, b, c, x[0], S32, 0xEAA127FA);
-        c = HH(c, d, a, b, x[3], S33, 0xD4EF3085);
-        b = HH(b, c, d, a, x[6], S34,  0x4881D05);
-        a = HH(a, b, c, d, x[9], S31, 0xD9D4D039);
-        d = HH(d, a, b, c, x[12], S32, 0xE6DB99E5);
-        c = HH(c, d, a, b, x[15], S33, 0x1FA27CF8);
-        b = HH(b, c, d, a, x[2], S34, 0xC4AC5665);
+        a = hh(a, b, c, d, x[5], S31, 0xFFFA3942);
+        d = hh(d, a, b, c, x[8], S32, 0x8771F681);
+        c = hh(c, d, a, b, x[11], S33, 0x6D9D6122);
+        b = hh(b, c, d, a, x[14], S34, 0xFDE5380C);
+        a = hh(a, b, c, d, x[1], S31, 0xA4BEEA44);
+        d = hh(d, a, b, c, x[4], S32, 0x4BDECFA9);
+        c = hh(c, d, a, b, x[7], S33, 0xF6BB4B60);
+        b = hh(b, c, d, a, x[10], S34, 0xBEBFBC70);
+        a = hh(a, b, c, d, x[13], S31, 0x289B7EC6);
+        d = hh(d, a, b, c, x[0], S32, 0xEAA127FA);
+        c = hh(c, d, a, b, x[3], S33, 0xD4EF3085);
+        b = hh(b, c, d, a, x[6], S34,  0x4881D05);
+        a = hh(a, b, c, d, x[9], S31, 0xD9D4D039);
+        d = hh(d, a, b, c, x[12], S32, 0xE6DB99E5);
+        c = hh(c, d, a, b, x[15], S33, 0x1FA27CF8);
+        b = hh(b, c, d, a, x[2], S34, 0xC4AC5665);
 
-        a = II(a, b, c, d, x[0], S41, 0xF4292244);
-        d = II(d, a, b, c, x[7], S42, 0x432AFF97);
-        c = II(c, d, a, b, x[14], S43, 0xAB9423A7);
-        b = II(b, c, d, a, x[5], S44, 0xFC93A039);
-        a = II(a, b, c, d, x[12], S41, 0x655B59C3);
-        d = II(d, a, b, c, x[3], S42, 0x8F0CCC92);
-        c = II(c, d, a, b, x[10], S43, 0xFFEFF47D);
-        b = II(b, c, d, a, x[1], S44, 0x85845DD1);
-        a = II(a, b, c, d, x[8], S41, 0x6FA87E4F);
-        d = II(d, a, b, c, x[15], S42, 0xFE2CE6E0);
-        c = II(c, d, a, b, x[6], S43, 0xA3014314);
-        b = II(b, c, d, a, x[13], S44, 0x4E0811A1);
-        a = II(a, b, c, d, x[4], S41, 0xF7537E82);
-        d = II(d, a, b, c, x[11], S42, 0xBD3AF235);
-        c = II(c, d, a, b, x[2], S43, 0x2AD7D2BB);
-        b = II(b, c, d, a, x[9], S44, 0xEB86D391);
+        a = ii(a, b, c, d, x[0], S41, 0xF4292244);
+        d = ii(d, a, b, c, x[7], S42, 0x432AFF97);
+        c = ii(c, d, a, b, x[14], S43, 0xAB9423A7);
+        b = ii(b, c, d, a, x[5], S44, 0xFC93A039);
+        a = ii(a, b, c, d, x[12], S41, 0x655B59C3);
+        d = ii(d, a, b, c, x[3], S42, 0x8F0CCC92);
+        c = ii(c, d, a, b, x[10], S43, 0xFFEFF47D);
+        b = ii(b, c, d, a, x[1], S44, 0x85845DD1);
+        a = ii(a, b, c, d, x[8], S41, 0x6FA87E4F);
+        d = ii(d, a, b, c, x[15], S42, 0xFE2CE6E0);
+        c = ii(c, d, a, b, x[6], S43, 0xA3014314);
+        b = ii(b, c, d, a, x[13], S44, 0x4E0811A1);
+        a = ii(a, b, c, d, x[4], S41, 0xF7537E82);
+        d = ii(d, a, b, c, x[11], S42, 0xBD3AF235);
+        c = ii(c, d, a, b, x[2], S43, 0x2AD7D2BB);
+        b = ii(b, c, d, a, x[9], S44, 0xEB86D391);
 
         state[0] += a;
         state[1] += b;
