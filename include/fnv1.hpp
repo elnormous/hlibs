9d8fdaeaffda1cf2e253d9ba163f0e20331dd520
Elviss Strazdins <elviss@elviss.lv>
2019-09-22 01:02:29 +0300

    Add fnv utility functions to anonymous namespace
    Set macOS deployment target to 10.10

--- include/fnv1.hpp
+++ include/fnv1.hpp
@@ -9,20 +9,23 @@
 
 namespace fnv1
 {
-    template <typename T> T prime() noexcept;
-    template <typename T> T offsetBasis() noexcept;
+    namespace
+    {
+        template <typename T> T prime() noexcept;
+        template <typename T> T offsetBasis() noexcept;
 
-    template <>
-    constexpr uint32_t prime() noexcept { return 16777619u; }
+        template <>
+        constexpr uint32_t prime() noexcept { return 16777619u; }
 
-    template <>
-    constexpr uint32_t offsetBasis() noexcept { return 2166136261u; }
+        template <>
+        constexpr uint32_t offsetBasis() noexcept { return 2166136261u; }
 
-    template <>
-    constexpr uint64_t prime() noexcept { return 1099511628211u; }
+        template <>
+        constexpr uint64_t prime() noexcept { return 1099511628211u; }
 
-    template <>
-    constexpr uint64_t offsetBasis() noexcept { return 14695981039346656037u; }
+        template <>
+        constexpr uint64_t offsetBasis() noexcept { return 14695981039346656037u; }
+    }
 
     template <typename Result, typename Iterator>
     constexpr Result hash(const Iterator i, const Iterator end,
