cf2f11aa7d5e3efae05450445ff14f455fecf5be
Elviss Strazdins <elviss@elviss.lv>
2020-01-18 03:13:33 +0200

    Rename FNV constant getters to verbs for consistency

--- include/fnv1.hpp
+++ include/fnv1.hpp
@@ -11,27 +11,39 @@ namespace fnv1
 {
     inline namespace detail
     {
-        template <typename T> constexpr T prime() noexcept;
-        template <typename T> constexpr T offsetBasis() noexcept;
+        template <typename T> constexpr T getPrime() noexcept;
+        template <typename T> constexpr T getOffsetBasis() noexcept;
 
         template <>
-        constexpr uint32_t prime<uint32_t>() noexcept { return 16777619U; }
+        constexpr uint32_t getPrime<uint32_t>() noexcept
+        {
+            return 16777619U;
+        }
 
         template <>
-        constexpr uint32_t offsetBasis<uint32_t>() noexcept { return 2166136261U; }
+        constexpr uint32_t getOffsetBasis<uint32_t>() noexcept
+        {
+            return 2166136261U;
+        }
 
         template <>
-        constexpr uint64_t prime<uint64_t>() noexcept { return 1099511628211U; }
+        constexpr uint64_t getPrime<uint64_t>() noexcept
+        {
+            return 1099511628211U;
+        }
 
         template <>
-        constexpr uint64_t offsetBasis<uint64_t>() noexcept { return 14695981039346656037U; }
+        constexpr uint64_t getOffsetBasis<uint64_t>() noexcept
+        {
+            return 14695981039346656037U;
+        }
     }
 
     template <typename Result, typename Iterator>
     constexpr Result hash(const Iterator i, const Iterator end,
-                          const Result result = offsetBasis<Result>()) noexcept
+                          const Result result = getOffsetBasis<Result>()) noexcept
     {
-        return (i != end) ? hash(i + 1, end, (result * prime<Result>()) ^ *i) : result;
+        return (i != end) ? hash(i + 1, end, (result * getPrime<Result>()) ^ *i) : result;
     }
 
     template <typename Result, typename T>
