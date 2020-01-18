2a20a5659d208edc7e2c049d7351b2831d4ac6cc
Elviss Strazdins <elviss@elviss.lv>
2020-01-18 03:15:11 +0200

    Add the missing constexpr

--- include/crc.hpp
+++ include/crc.hpp
@@ -140,7 +140,7 @@ namespace crc
             0xB3667A2E, 0xC4614AB8, 0x5D681B02, 0x2A6F2B94, 0xB40BBE37, 0xC30C8EA1, 0x5A05DF1B, 0x2D02EF8D
         };
 
-        template <> uint32_t getEntry<uint32_t>(uint8_t i)
+        template <> constexpr uint32_t getEntry<uint32_t>(uint8_t i)
         {
             return table32[i];
         }
