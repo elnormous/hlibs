848c8244cc08d332411f2ff9483ae900587cea2a
Elviss Strazdins <elviss@elviss.lv>
2020-08-26 01:25:51 +0300

    Switch to C++14

--- include/fnv1.hpp
+++ include/fnv1.hpp
@@ -11,39 +11,21 @@ namespace fnv1
 {
     inline namespace detail
     {
-        template <typename T> constexpr T getPrime() noexcept;
-        template <typename T> constexpr T getOffsetBasis() noexcept;
+        template <typename T> constexpr T prime;
+        template <typename T> constexpr T offsetBasis;
 
-        template <>
-        constexpr std::uint32_t getPrime<std::uint32_t>() noexcept
-        {
-            return 16777619U;
-        }
+        template <> constexpr std::uint32_t prime<std::uint32_t> = 16777619U;
+        template <> constexpr std::uint32_t offsetBasis<std::uint32_t> = 2166136261U;
 
-        template <>
-        constexpr std::uint32_t getOffsetBasis<std::uint32_t>() noexcept
-        {
-            return 2166136261U;
-        }
-
-        template <>
-        constexpr std::uint64_t getPrime<std::uint64_t>() noexcept
-        {
-            return 1099511628211U;
-        }
-
-        template <>
-        constexpr std::uint64_t getOffsetBasis<std::uint64_t>() noexcept
-        {
-            return 14695981039346656037U;
-        }
+        template <> constexpr std::uint64_t prime<std::uint64_t> = 1099511628211U;
+        template <> constexpr std::uint64_t offsetBasis<std::uint64_t> = 14695981039346656037U;
     }
 
     template <typename Result, typename Iterator>
     constexpr Result hash(const Iterator i, const Iterator end,
-                          const Result result = getOffsetBasis<Result>()) noexcept
+                          const Result result = offsetBasis<Result>) noexcept
     {
-        return (i != end) ? hash(i + 1, end, (result * getPrime<Result>()) ^ *i) : result;
+        return (i != end) ? hash(i + 1, end, (result * prime<Result>) ^ *i) : result;
     }
 
     template <typename Result, typename T>
