d15b8c2d5ef9be8c67b3c9f205ea3f7f297888e6
Elviss Strazdins <elviss@elviss.lv>
2020-08-28 03:10:37 +0300

    Switch back to using functions instead of variable templates because of one definition rule

--- include/fnv1.hpp
+++ include/fnv1.hpp
@@ -11,21 +11,21 @@ namespace fnv1
 {
     inline namespace detail
     {
-        template <typename T> constexpr T prime = 0;
-        template <typename T> constexpr T offsetBasis = 0;
+        template <typename T> T prime();
+        template <typename T> T offsetBasis();
 
-        template <> constexpr std::uint32_t prime<std::uint32_t> = 16777619U;
-        template <> constexpr std::uint32_t offsetBasis<std::uint32_t> = 2166136261U;
+        template <> constexpr std::uint32_t prime<std::uint32_t>() { return 16777619U; }
+        template <> constexpr std::uint32_t offsetBasis<std::uint32_t>() { return 2166136261U; }
 
-        template <> constexpr std::uint64_t prime<std::uint64_t> = 1099511628211ULL;
-        template <> constexpr std::uint64_t offsetBasis<std::uint64_t> = 14695981039346656037ULL;
+        template <> constexpr std::uint64_t prime<std::uint64_t>() { return 1099511628211ULL; }
+        template <> constexpr std::uint64_t offsetBasis<std::uint64_t>() { return 14695981039346656037ULL; }
     }
 
     template <typename Result, typename Iterator>
     constexpr Result hash(const Iterator i, const Iterator end,
-                          const Result result = offsetBasis<Result>) noexcept
+                          const Result result = offsetBasis<Result>()) noexcept
     {
-        return (i != end) ? hash(i + 1, end, (result * prime<Result>) ^ *i) : result;
+        return (i != end) ? hash(i + 1, end, (result * prime<Result>()) ^ *i) : result;
     }
 
     template <typename Result, typename T>
