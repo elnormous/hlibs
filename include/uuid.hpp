130926c1d06a3af84ecd5e8ea26a6b763336fd90
Elviss Strazdins <elviss@elviss.lv>
2022-01-08 00:44:40 +0200

    Pass random engine to UUID and generate 32-bit random

--- include/uuid.hpp
+++ include/uuid.hpp
@@ -22,32 +22,26 @@ namespace uuid
         std::array<std::uint8_t, 6> node;
     };
 
-    [[nodiscard]] inline Uuid generate()
+    std::mt19937 mt{std::random_device{}()};
+
+    template <class RandomEngine = std::mt19937>
+    [[nodiscard]] Uuid generate(RandomEngine& re = mt)
     {
-        static std::random_device rd;
-        static std::mt19937_64 mt{rd()};
+        const auto timeLow = static_cast<std::uint32_t>(re());
+        const auto timeHigh = static_cast<std::uint32_t>(re());
 
-        const std::uint64_t randomTime = mt();
-
-        const std::uint32_t timeLow = ((randomTime >> 24) & 0x000000FFU) |
-            ((randomTime >> 8) & 0x0000FF00U) |
-            ((randomTime << 8) & 0x00FF0000U) |
-            ((randomTime << 24) & 0xFF000000U);
-
-        const auto timeMid = static_cast<std::uint16_t>(((randomTime >> 40) & 0x00FFU) |
-                                                        ((randomTime >> 24) & 0xFF00U));
+        const auto timeMid = static_cast<std::uint16_t>((timeHigh >> 16) & 0xFFFFU);
 
         const auto timeHiAndVersion = static_cast<std::uint16_t>(((0x04U << 12) & 0xF000U) |
-                                                                 ((randomTime >> 56) & 0x00FFU) |
-                                                                 ((randomTime >> 40) & 0x0F00U));
+                                                                 (timeHigh & 0xFFFFU));
 
-        const auto clockSequence = static_cast<std::uint16_t>(mt() & 0x3FFFU); // 14-bit random
+        const auto clockSequence = static_cast<std::uint32_t>(re());
 
         const auto clockSeqHiAndReserved = static_cast<std::uint8_t>(0x80U | // bit 6 and 7
-                                                                     ((clockSequence >> 8) & 0x3FU));
-        const auto clockSeqLow = static_cast<std::uint8_t>(clockSequence & 0xFFU);
+                                                                     ((clockSequence >> 24) & 0x3FU));
+        const auto clockSeqLow = static_cast<std::uint8_t>((clockSequence >> 16) & 0xFFU);
 
-        const auto random = mt();
+        const auto node = static_cast<std::uint32_t>(re());
 
         return {
             timeLow,
@@ -56,19 +50,20 @@ namespace uuid
             clockSeqHiAndReserved,
             clockSeqLow,
             {
-                static_cast<std::uint8_t>(random >> 48),
-                static_cast<std::uint8_t>(random >> 40),
-                static_cast<std::uint8_t>(random >> 32),
-                static_cast<std::uint8_t>(random >> 24),
-                static_cast<std::uint8_t>(random >> 16),
-                static_cast<std::uint8_t>(random >> 0)
+                static_cast<std::uint8_t>(clockSequence >> 8),
+                static_cast<std::uint8_t>(clockSequence >> 0),
+                static_cast<std::uint8_t>(node >> 24),
+                static_cast<std::uint8_t>(node >> 16),
+                static_cast<std::uint8_t>(node >> 8),
+                static_cast<std::uint8_t>(node >> 0)
             }
         };
     }
 
-    [[nodiscard]] inline std::array<std::uint8_t, 16> generateArray()
+    template <class RandomEngine = std::mt19937>
+    [[nodiscard]] std::array<std::uint8_t, 16> generateArray(RandomEngine& re = mt)
     {
-        const auto uuid = generate();
+        const auto uuid = generate(re);
 
         return std::array<std::uint8_t, 16>{
             static_cast<std::uint8_t>(uuid.timeLow >> 24),
@@ -94,11 +89,12 @@ namespace uuid
         };
     }
 
-    [[nodiscard]] inline std::string generateString()
+    template <class RandomEngine = std::mt19937>
+    [[nodiscard]] std::string generateString(RandomEngine& re = mt)
     {
         constexpr char digits[] = "0123456789abcdef";
 
-        const auto uuid = generate();
+        const auto uuid = generate(re);
 
         return {
             digits[(uuid.timeLow >> 28) & 0x0FU],
